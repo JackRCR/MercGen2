f89341187f4955443466bfdf6656f629074df256
Carefulrogue <carefulrogue@gmail.com>
2022-08-13 23:12:42 -0500

    Added Database folder and subfolders.

---
+++ Database/Models/ItemListModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MercGen2.Database.Models
+{
+	using System.ComponentModel.DataAnnotations;
+	internal class ItemListModel
+	{
+		[Key]
+		string itemName { get; set; }
+		[Key]
+		ulong channelID { get; set; }
+		int quantity { get; set; }
+	}//end of class
+}//end of namespace
